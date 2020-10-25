16fea2439dab3a18b211d3bb4cffbd55268ed665
Sander Beekhuis <sander.beekhuis@live.nl>
2020-10-25 13:21:54 +0100

    Implement calculate_frequency_for_word

--- src/word_frequency_analysis.py
+++ src/word_frequency_analysis.py
@@ -1,11 +1,24 @@
 import re
 
+
 class WordFrequencyAnalyzer:
     # The existence of this class does not add any value over a couple of methods in a module.
     # It is only created to satisfy the task requirements
 
     @staticmethod
     def calculate_highest_frequency(text):
+        freqencies = WordFrequencyAnalyzer._analyze_text(text)
+        return max(freqencies.values())
+
+    @staticmethod
+    def calculate_frequency_for_word(text, word):
+        frequencies = WordFrequencyAnalyzer._analyze_text(text)
+        word = str.lower(word)
+
+        return dict.get(frequencies, word, 0)
+
+    @staticmethod
+    def _analyze_text(text):
         lower_text = str.lower(text)
         words = re.findall("[a-zA-Z]+", lower_text)
 
@@ -13,4 +26,4 @@ class WordFrequencyAnalyzer:
         for word in words:
             frequencies[word] = frequencies.setdefault(word, 0) + 1
 
-        return max(frequencies.values())
+        return frequencies
--- test/test_word_frequency_analysis.py
+++ test/test_word_frequency_analysis.py
@@ -2,7 +2,7 @@ import src.word_frequency_analysis as wfa
 import pytest
 
 
-@pytest.mark.parametrize("text,highest_freq", [
+@pytest.mark.parametrize("text,expected", [
         ("oneWord other words", 1),
         ("two two", 2),
         ("tabs\ttabs", 2),
@@ -11,5 +11,15 @@ import pytest
         ("non-whitespace-separator non", 2),
         ("The sun shines over the lake", 2)
     ])
-def test_word_frequency_analysis(text, highest_freq):
-    assert wfa.WordFrequencyAnalyzer.calculate_highest_frequency(text) == highest_freq;
+def calculate_highest_frequency_test(text, expected):
+    assert wfa.WordFrequencyAnalyzer.calculate_highest_frequency(text) == expected
+
+
+@pytest.mark.parametrize("text,word,expected", [
+    ("This is a sentence", "is", 1),
+    ("This is a sentence", "not", 0),
+    ("The sun shines over the lake", "the", 2),
+    ("The sun shines over the lake", "tHe", 2),
+])
+def test(text, word, expected):
+    assert wfa.WordFrequencyAnalyzer.calculate_frequency_for_word(text, word) == expected
